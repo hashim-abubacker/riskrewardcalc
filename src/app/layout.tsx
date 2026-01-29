f77b7db7d55c245ca41cddd41392d65e95b0b106
hashim-abubacker <130749153+hashim-abubacker@users.noreply.github.com>
2026-01-29 19:16:29 +0530

    Microsoft Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -115,6 +115,17 @@ export default function RootLayout({
             gtag('config', 'G-W1ZCGRKYJC');
           `}
         </Script>
+
+        {/* Microsoft Clarity - Heatmaps & Session Recordings */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "v91v1wz9ej");
+          `}
+        </Script>
       </body>
     </html>
   );
